f1bcb60539cd441d009b2b5069e5e832e99c9048
bogyuni <bogyuni@hotmail.com>
2022-04-19 13:32:46 +0900

    collision action

--- asset/js/action.js
+++ asset/js/action.js
@@ -6,35 +6,69 @@ const hero = document.querySelector('.hero');
 const enemy = document.querySelector('.enemy');
 
 const heroStat = {
+	posTop : parseInt(winHeight / 2 / tileSize) * tileSize,
 	posLeft : parseInt(winWidth / 2 / tileSize) * tileSize,
-	posTop : parseInt(winHeight / 2 / tileSize) * tileSize
+	direction : hero.dataset.direction = 'right'
 }
 const enemyStat = {
-	posLeft : parseInt(winWidth / 2 / tileSize) * tileSize,
-	posTop : parseInt(winHeight / 4 / tileSize) * tileSize
+	posTop : parseInt(winHeight / 4 / tileSize) * tileSize,
+	posLeft : parseInt(winWidth / 2 / tileSize) * tileSize
 }
 
-hero.style.left = heroStat.posLeft+'px';
 hero.style.top = heroStat.posTop+'px';
-enemy.style.left = enemyStat.posLeft+'px';
+hero.style.left = heroStat.posLeft+'px';
 enemy.style.top = enemyStat.posTop+'px';
+enemy.style.left = enemyStat.posLeft+'px';
+
+function positionCheck(character, coordinate) {
+	let currentCoordinatePx = window.getComputedStyle(character).getPropertyValue(coordinate);
+	let currentCoordinate = parseInt(currentCoordinatePx.split('px')[0]);
+	return currentCoordinate;
+}
 
 
 window.onkeydown = (e) => {
-	let currentLeftPx = window.getComputedStyle(hero).getPropertyValue('left');
-	let currentLeft = parseInt(currentLeftPx.split('px')[0]);
-	let currentTopPx = window.getComputedStyle(hero).getPropertyValue('top');
-	let currentTop = parseInt(currentTopPx.split('px')[0]);
+	let currentTop = parseInt(positionCheck(hero, 'top'));
+	let currentLeft = parseInt(positionCheck(hero, 'left'));
 
-	console.log(currentTop+tileSize, e);
-
-	if (e.key === 'ArrowLeft') {
+	if (e.key === 'ArrowLeft')
+	{
 		hero.style.left = `${currentLeft-tileSize}px`;
-	} else if (e.key === 'ArrowRight') {
-		hero.style.left = `${currentLeft+tileSize}px`;
-	} else if (e.key === 'ArrowUp') {
-		hero.style.top = `${currentTop-tileSize}px`;
-	} else if (e.key === 'ArrowDown') {
-		hero.style.top = `${currentTop+tileSize}px`;
+		hero.dataset.direction = 'left';
 	}
+	else if (e.key === 'ArrowRight')
+	{
+		hero.style.left = `${currentLeft+tileSize}px`;
+		hero.dataset.direction = 'right';
+	}
+	else if (e.key === 'ArrowUp')
+	{
+		hero.style.top = `${currentTop-tileSize}px`;
+		hero.dataset.direction = 'up';
+	}
+	else if (e.key === 'ArrowDown')
+	{
+		hero.style.top = `${currentTop+tileSize}px`;
+		hero.dataset.direction = 'down';
+	}
+
+	hero.innerHTML = hero.dataset.direction;
+}
+
+window.onkeyup = (e) => {
+	let currentTop = parseInt(positionCheck(hero, 'top'));
+	let currentLeft = parseInt(positionCheck(hero, 'left'));
+	let enemyTop = parseInt(positionCheck(enemy, 'top'));
+	let enemyLeft = parseInt(positionCheck(enemy, 'left'));
+
+	if( enemyTop == currentTop && enemyLeft == currentLeft ) 
+	{
+		alert('Warnning!!');
+	}
+	else
+	{
+		console.log('top:'+(currentTop), 'left:'+(currentLeft));
+	}
+
+
 }
