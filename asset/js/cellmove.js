6b64e63ca70dfa49057769157b90560f588e38b5
bogyuni <q11343@Q11343-2PN1170200003.local>
2024-03-12 18:02:12 +0900

    protfolio update

--- asset/js/cellmove.js
+++ asset/js/cellmove.js
@@ -2,29 +2,31 @@ import {setSubStatus} from './substatus.js';
 
 const winWidth = window.innerWidth;
 const winHeight = window.innerHeight;
-const tileSize = 100;
-const delayTime = 201; // 0.2s
+const tileSize = 50;
+const delayTime = 101; // 0.2s
 let moveCheck = true; // 캐릭터 이동 후 딜레이 체크
 const viewWidth = 1000;
 const viewHeight = 700;
 
 const heroCM = document.querySelector('#heroCM');
-const enemy = document.querySelector('.enemy');
+const exit = document.querySelector('#exit');
 
 const heroCMStat = {
 	posTop : parseInt(viewHeight / 2 / tileSize) * tileSize,
 	posLeft : parseInt(viewWidth / 2 / tileSize) * tileSize,
 	direction : heroCM.dataset.direction = 'right'
 }
-const enemyStat = {
-	posTop : parseInt(viewHeight / 4 / tileSize) * tileSize,
-	posLeft : parseInt(viewWidth / 2 / tileSize) * tileSize
+const exitStat = {
+	// posTop : parseInt(viewHeight / 4 / tileSize) * tileSize,
+	// posLeft : parseInt(viewWidth / 2 / tileSize) * tileSize
+	posTop : exit.offsetTop,
+	posLeft : exit.offsetLeft,
 }
 
 heroCM.style.top = heroCMStat.posTop+'px';
 heroCM.style.left = heroCMStat.posLeft+'px';
-enemy.style.top = enemyStat.posTop+'px';
-enemy.style.left = enemyStat.posLeft+'px';
+// exit.style.top = exitStat.posTop+'px';
+// exit.style.left = exitStat.posLeft+'px';
 
 function positionCheck(character, coordinate) {
 	const currentCoordinatePx = window.getComputedStyle(character).getPropertyValue(coordinate);
@@ -38,7 +40,7 @@ function cellmoveKeydown(key) {
 		const currentTop = parseInt(positionCheck(heroCM, 'top'));
 		const currentLeft = parseInt(positionCheck(heroCM, 'left'));
 
-		console.log(currentLeft, currentTop)
+		// console.log(currentLeft, currentTop)
 
 		if (key === 'ArrowLeft') {
 			if( currentLeft > 0) {
@@ -82,17 +84,20 @@ function cellmoveKeyup(key) {
 		setTimeout(function(){
 			const currentTop = parseInt(positionCheck(heroCM, 'top'));
 			const currentLeft = parseInt(positionCheck(heroCM, 'left'));
-			const enemyTop = parseInt(positionCheck(enemy, 'top'));
-			const enemyLeft = parseInt(positionCheck(enemy, 'left'));
-		
-			if( enemyTop == currentTop && enemyLeft == currentLeft ) {
+			// const exitTop = parseInt(positionCheck(exit, 'top'));
+			// const exitLeft = parseInt(positionCheck(exit, 'left'));
+			const exitTop = tileSize * 1;
+			const exitLeft = tileSize * 10;
+
+			if( exitTop == currentTop && exitLeft == currentLeft ) {
 				setSubStatus('beltscroll');
 				heroCM.style.top = heroCMStat.posTop+'px';
 				heroCM.style.left = heroCMStat.posLeft+'px';
 	
 				console.log('Acecss !!');
 			} else {
-				// console.log('top:'+(currentTop), 'left:'+(currentLeft));
+				console.log(exitTop, exitLeft, typeof exitLeft, typeof currentLeft);
+				console.log('top:'+(currentTop), 'left:'+(currentLeft));
 			}
 		}, delayTime);
 }
