976b3e1d31b46dc365c8e80203d2cf7cd85bbbc3
bogyuni <q11343@Q11343-2PN1170200003.local>
2024-03-22 18:12:09 +0900

    Weather ADD, upgrade

--- asset/js/weather.js
+++ asset/js/weather.js
@@ -1,3 +1,33 @@
+/**
+ * weather code
+ * @param {*} code
+ * 2xx : Thunderstorm 뇌우
+ * 3xx : Drizzle 이슬비
+ * 5xx : Rain 비
+ * 6xx : Snow 눈
+ * 7xx : Atmosphere 안개
+ * 800 : Clear 맑음
+ * 8xx : Clouds 구름
+ */
+function weatherDisplay(code) {
+  if (code > 800) {
+    console.log(code+' : 구름');
+  } else if (code >= 200 && code < 300) {
+    console.log(code+' : 뇌우');
+  } else if (code >= 300 && code < 400) {
+    console.log(code+' : 이슬비');
+  } else if (code >= 500 && code < 600) {
+    console.log(code+' : 비');
+  } else if (code >= 600 && code < 700) {
+    console.log(code+' : 눈');
+  } else if (code >= 700 && code < 800) {
+    console.log(code+' : 안개');
+  } else {
+    console.log(code+' : 맑음');
+  }
+}
+
+
 const API_KEY = '652eb13f28a2b10354f103edee6fd79b';
 const getWeather = (lat, lon) => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`)
@@ -5,12 +35,13 @@ const getWeather = (lat, lon) => {
     return response.json();
   })
   .then((json) => {
-    console.log(json);
+    // console.log(json);
     const temperature = json.main.temp;
     const place = json.name;
     const description = json.weather[0].description;
-
-    document.querySelector('#weatherData').innerHTML = `${temperature} // ${place} // ${description}`
+    const id = json.weather[0].id;
+    document.querySelector('#weatherData').innerHTML = `온도: ${temperature} // 지역: ${place} // 날씨: ${description} // 아이디: ${id}`;
+    weatherDisplay(id);
   })
   .catch((error) => {
     alert(error);
@@ -18,15 +49,12 @@ const getWeather = (lat, lon) => {
 }
 
 const success = (position) => {
-  console.log(position);
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-
   getWeather(latitude, longitude);
 }
 const fail = () => {
   alert("좌표를 받아올 수 없음");
 }
-
 
 navigator.geolocation.getCurrentPosition(success, fail);
