8bd3b75cfa4cdcc4685b01ca5068ba93eda8f61d
bogyuni <q11343@Q11343-2PN1170200003.local>
2024-03-25 17:41:34 +0900

    weather data add

--- asset/js/weather.js
+++ asset/js/weather.js
@@ -1,3 +1,23 @@
+const wData = {
+  clear: './asset/data/clear.html',
+  cloud: './asset/data/cloud.html',
+  rain: './asset/data/rain.html',
+  snow: './asset/data/snow.html',
+};
+
+const wDataCall = (uri) => {
+  fetch(uri, { method : 'get'})
+  .then((response) => {
+    return response.text();
+  })
+  .then((text) => {
+    document.querySelector('#weatherContent').insertAdjacentHTML('beforeend', text);
+  })
+  .catch((error) => {
+    alert(error);
+  });
+}
+
 /**
  * weather code
  * @param {*} code
@@ -12,21 +32,27 @@
 function weatherDisplay(code) {
   if (code > 800) {
     console.log(code+' : 구름');
+    wDataCall(wData.cloud);
   } else if (code >= 200 && code < 300) {
     console.log(code+' : 뇌우');
+    wDataCall(wData.rain);
   } else if (code >= 300 && code < 400) {
     console.log(code+' : 이슬비');
+    wDataCall(wData.rain);
   } else if (code >= 500 && code < 600) {
     console.log(code+' : 비');
+    wDataCall(wData.rain);
   } else if (code >= 600 && code < 700) {
     console.log(code+' : 눈');
+    wDataCall(wData.rain);
   } else if (code >= 700 && code < 800) {
     console.log(code+' : 안개');
+    wDataCall(wData.cloud);
   } else {
     console.log(code+' : 맑음');
+    wDataCall(wData.clear);
   }
 }
-
 
 const API_KEY = '652eb13f28a2b10354f103edee6fd79b';
 const getWeather = (lat, lon) => {
@@ -35,7 +61,6 @@ const getWeather = (lat, lon) => {
     return response.json();
   })
   .then((json) => {
-    // console.log(json);
     const temperature = json.main.temp;
     const place = json.name;
     const description = json.weather[0].description;
